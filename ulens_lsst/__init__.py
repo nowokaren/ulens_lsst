3690417390d2d3759eb01474f38af6d11f7279d0
Karen <nowo.karen@gmail.com>
2025-09-07 17:16:43 +0000

    Fix package imports in __init__.py

--- ulens_lsst/__init__.py
+++ ulens_lsst/__init__.py
@@ -5,3 +5,5 @@ This package provides tools for simulating microlensing events using LSST data
 (DP0, DP1, or rubin_sim), designed for versatile experimentation.
 """
 __version__ = "1.0.0"
+from .catalogs_utils import Catalog
+from .simulation_pipeline import SimPipeline
